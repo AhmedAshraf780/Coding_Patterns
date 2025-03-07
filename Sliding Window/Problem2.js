7bf76b7aa3cf57760a516576451b812563dafa2d
AhmedAshraf780 <amedashraf780@gmail.com>
2025-03-07 17:40:19 +0200

    Second Problem

---
+++ Sliding Window/Problem2.js
@@ -0,0 +1,35 @@
+// Link
+// https://leetcode.com/problems/longest-harmonious-subsequence/?envType=problem-list-v2&envId=sliding-window
+//
+//
+
+// O(n)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function(nums) {
+
+    var map = new Map();
+    var maxL = 0;
+
+    // Count frequency of each number
+    for(var num of nums){
+      map.set(num,(map.get(num)|0)+1);
+    }
+
+    // Combine each num count with num + 1 count 
+    // And calc the maxLength
+
+    for(const[num,count] of map){
+
+      if(map.has(num+1)){
+
+        maxL = Math.max(maxL,count + map.get(num+1))
+      }
+    }
+    return maxL;
+    
+};
+
